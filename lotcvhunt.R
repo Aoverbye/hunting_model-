94e3d4a43f9c4f0e24268af077b69ef5b520f1c1
matteo-torres <matteotorres27@gmail.com>
2025-06-03 18:54:45 -0700

    update format

--- lotcvhunt.R
+++ lotcvhunt.R
@@ -1,16 +1,27 @@
-# hunting_model_functions.R
-
-library(deSolve)
-
-# Hunting model function
+#' Hunting Model Function
+#'
+#' @param t 
+#' @param pop 
+#' @param pars 
+#'
+#' @return
+#' @export
+#'
+#' @examples
 lotvhunt <- function(t, pop, pars) {
+  
   with(as.list(c(pars, pop)), {
+    
     hunt <- ifelse(prey >= prey_min, rhunt * prey, 0)
+    
     hunt <- min(hunt, prey)  # prevent overharvest
     
     dprey <- rprey * (1 - prey / K) * prey - alpha * prey * pred - hunt
+    
     dpred <- eff * alpha * prey * pred - pmort * pred
     
     return(list(c(dprey, dpred)))
+    
   })
+  
 }
