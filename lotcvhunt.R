d3a87cb7ce9c64fad11ae48c87c69f3493be9057
Aoverbye <amandaoverbye@gmail.com>
2025-06-03 22:23:33 -0500

    Worked on stability parameter, population  and added line graphs.

--- lotcvhunt.R
+++ lotcvhunt.R
@@ -1,27 +1,25 @@
-#' Hunting Model Function
+#' Hunting Predator-Prey Model
 #'
-#' @param t 
-#' @param pop 
-#' @param pars 
+#' Computes prey and predator population changes with hunting.
 #'
-#' @return
+#' @param t Time (from ODE solver).
+#' @param pop Named vector of populations: prey and pred.
+#' @param pars Named list of parameters.
+#'
+#' @return List of population change rates for prey and predator.
 #' @export
 #'
 #' @examples
+
 lotvhunt <- function(t, pop, pars) {
-  
   with(as.list(c(pars, pop)), {
     
-    hunt <- ifelse(prey >= prey_min, rhunt * prey, 0)
-    
-    hunt <- min(hunt, prey)  # prevent overharvest
+    hunt <- ifelse(prey >= prey_min, rhunt * prey^1.2, 0)
+    hunt <- min(hunt, prey)
     
     dprey <- rprey * (1 - prey / K) * prey - alpha * prey * pred - hunt
-    
     dpred <- eff * alpha * prey * pred - pmort * pred
     
     return(list(c(dprey, dpred)))
-    
   })
-  
 }
